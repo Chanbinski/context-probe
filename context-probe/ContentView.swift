1a327d8fbea35afb3772eb9f8c1c067ab46c23b3
Chanbin Park <chanbin.park@berkeley.edu>
2025-07-02 10:30:08 +0900

    Initial Commit

---
+++ context-probe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  context-probe
+//
+//  Created by Chanbin Park on 7/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ context-probe/context_probeApp.swift
@@ -0,0 +1,17 @@
+//
+//  context_probeApp.swift
+//  context-probe
+//
+//  Created by Chanbin Park on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct context_probeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
